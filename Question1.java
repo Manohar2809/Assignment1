6c48728690ad27ef63388fd9c3d54e7cdbb9bdbf
Manohar2809 <125918354+Manohar2809@users.noreply.github.com>
2023-02-21 01:11:03 +0530

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,68 @@
+package firstassignment;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		int n = 11;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (j == (3 * n / 4) && i <= n - 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			for (int j = 0; j < n; j++) {
+				if (i == j || j == 0 || j == n - 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			for (int j = 0; j < n; j++) {
+				if (j == (n / 4) - 1 || (i == 0 && j >= n / 4 && j < n - 1) || (i == n - 1 && j >= n / 4 && j < n - 1)
+						|| (i == (n - 1) / 2 && j >= n / 4 && j < n - 1)) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+
+			for (int j = 0; j < n; j++) {
+				if (j == 0 && i < n - 1 || j == (3 * n) / 4 && i < n - 1
+						|| i == n - 1 && j < n - 1 && j < (3 * n) / 4 && j > 0) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			for (int j = 0; j < n; j++) {
+				if (i == 0 && j < n - 1 || j == 0 || i == (n - 1) / 2 && j < (n - 1)
+						|| j == (n - 1) && i < (n - 1) / 2 && i > 0 || (i == j) && i > n / 2 && j > n / 2) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			for (int j = 0; j < n; j++) {
+				if (j == n / 4 && i > 0 && i < n - 1 || j == (3 * n) / 4 && i > 0 && i < n - 1
+						|| i == 0 && j < (3 * n) / 4 && j > n / 4 || i == n - 1 && j < (3 * n) / 4 && j > n / 4) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			for (int j = 0; j < n; j++) {
+				if (i == j || j == 0 || j == n - 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
